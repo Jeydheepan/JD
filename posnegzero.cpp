62dcc8d123e33c4fc175ea6a53932d7d4eef1816
Jeydheepan <54067193+Jeydheepan@users.noreply.github.com>
2019-08-13 18:22:18 +0530

    Create posnegzero.cpp

---
+++ posnegzero.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cin>>a;
+  if(a>o)
+  {
+     cout<<"Positive";
+  }
+  else if(a<0)
+  {
+      cout<<"Negative";
+  }
+  else
+  {
+      cout<<"Zero";
+  }
+}
