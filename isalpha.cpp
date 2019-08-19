55a84db512e91e45cfa01dcc536bbd8f0fabc4aa
Jeydheepan <54067193+Jeydheepan@users.noreply.github.com>
2019-08-19 21:52:21 +0530

    Create isalpha.cpp

---
+++ isalpha.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a;
+    cin>>a;
+    if(isalpha(a))
+    {
+        cout<<"Alphabet";
+    }
+    else
+    {
+        cout<<"No";
+    }
+}
